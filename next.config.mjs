46166273d45b58e4f70454c442d228c14d3cf189
Aranna <melanieabalde0@gmail.com>
2025-01-21 01:23:38 +0800

    Push to Working Directory

--- next.config.mjs
+++ next.config.mjs
@@ -13,8 +13,20 @@ const nextConfig = {
     // Adding alias to Webpack configuration
     config.resolve.alias['@'] = path.resolve(__dirname); // Use __dirname here
     config.resolve.alias['@components'] = path.resolve(__dirname, 'app/components');
-    config.resolve.alias['@assets'] = path.resolve(__dirname, 'app/assets');
+    config.resolve.alias['@assets'] = path.resolve(__dirname, 'public/assets'); // Make sure this is where your assets are
     config.resolve.alias['@pages'] = path.resolve(__dirname, 'pages'); // Ensure correct path
+
+    // Add rule to handle font files (woff, woff2, etc.)
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 10000, // Adjust this value based on your file size
+          name: 'fonts/[name].[hash:8].[ext]',
+        },
+      },
+    });
 
     // Return the modified config
     return config;
