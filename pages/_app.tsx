94447bebb641a725d585eabd7759bc8cac0d5a38
Aranna <melanieabalde0@gmail.com>
2025-01-21 02:17:21 +0800

    Eslint Fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../app/styles/App.css";
 
 import { AppProps } from "next/app";
-import { Inter } from "next/font/google";
 import { ToastProvider } from "../components/ui/toast"; // Adjust import path based on your setup
 
 export default function App({ Component, pageProps }: AppProps) {
