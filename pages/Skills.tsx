c12aa3caa4e32707af8d700d04d7a77d07fb99c4
Aranna <melanieabalde0@gmail.com>
2025-09-06 19:36:37 +0800

    Done

--- pages/Skills.tsx
+++ pages/Skills.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FaDocker, FaGitAlt, FaNodeJs, FaVuejs } from "react-icons/fa";
+import { FaDocker, FaGitAlt, FaNodeJs, FaVuejs,  } from "react-icons/fa";
 import {
   SiDjango,
   SiMysql,
@@ -11,8 +11,6 @@ import {
   SiPostman,
   SiFastapi,
   SiSwagger,
-  SiFlutter,
-  
 } from "react-icons/si";
 import React from "react";
 import { motion } from "framer-motion";
@@ -24,7 +22,6 @@ type SkillSet = {
 
 const skillSets: SkillSet[] = [
   { Icon: FaVuejs, name: "VueJS" },
-  { Icon: SiFlutter, name: "Flutter" },
   { Icon: SiTypescript, name: "TypeScript" },
   { Icon: SiDjango, name: "Django" },
   { Icon: SiFastapi, name: "Fastapi" },
@@ -41,7 +38,7 @@ const skillSets: SkillSet[] = [
 const Skills: React.FC = () => {
   return (
     <section className="w-full px-4 sm:px-6 lg:px-8 max-w-[1200px] mx-auto py-8 overflow-visible">
-      {/* Headline (always left-aligned) */}
+      {/* Headline */}
       <div className="flex justify-start">
         <div className="inline-flex items-center text-xs font-semibold mb-6 px-3 py-1 rounded-lg border shadow-sm hover:shadow-md transition-all duration-300 bg-purple-900 text-white border-none dark:bg-purple-900/30 dark:text-purple-300 dark:border-purple-900/30 cursor-pointer">
           <h1 className="text-center">Skills</h1>
@@ -55,20 +52,17 @@ const Skills: React.FC = () => {
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.8, delay: 0.2 }}
       >
-        {/* 👇 Center on mobile, left on desktop */}
-        <div className="flex flex-wrap gap-2 py-3 rounded-lg px-0 justify-center md:justify-start items-center">
+        <div className="flex flex-wrap gap-4 py-6 rounded-lg px-0 justify-start items-center">
           {skillSets.map((skill, index) => (
             <motion.div
               key={index}
-              className="flex flex-col items-center p-2 rounded-lg bg-purple-900 dark:bg-gray-800/50 backdrop-blur-lg transition-all hover:scale-105 min-w-[80px]"
+              className="flex flex-col items-center p-3 rounded-lg bg-purple-900 dark:bg-gray-800/50 backdrop-blur-lg transition-all hover:scale-105 min-w-[80px]"
               whileHover={{ y: -5 }}
             >
-              {/* Icon */}
-              <div className="flex w-[20rem] justify-center items-center">
+              {/* Icon Centered inside the card */}
                 <div className="icon-container flex justify-center items-center">
                   <skill.Icon className="text-xl md:text-2xl mb-2 text-white" />
                 </div>
-              </div>
 
               {/* Label */}
               <div className="text-container">
